6a3745f1dfd16c0afaf4cbaa6eb5598b01cc241e
Harsha Addanki <63471542+harsha7addanki@users.noreply.github.com>
2020-08-11 11:43:39 -0400

    Add files via upload

---
+++ mrshell.py
@@ -0,0 +1,22 @@
+import os
+import json
+
+with open("settings.json") as settings_file:
+    settings = json.load(settings_file)
+    pro = settings['prompt']
+cmd = ""
+
+print("Welcome To The Python Shell (aka mrshell) v1.0.0")
+
+while True:
+    try:
+        cmd = input(pro)
+    except KeyboardInterrupt:
+        print("^C")
+    if cmd != "exit":
+        os.system(cmd)
+    elif cmd == "exit":
+        print("Closing mrshell ...")
+        os.system("sleep 3")
+        os.system("clear")
+        exit()
